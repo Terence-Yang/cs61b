ae907eaf0e7dd0fd711f86c6f1cb17ca955972ff
Yang Zou <yang0201@outlook.com>
2019-01-27 21:56:08 +0000

    add Body constructors

---
+++ proj0/Body.java
@@ -0,0 +1,26 @@
+public class Body {
+	public double xxPos;
+	public double yyPos;
+	public double xxVel;
+	public double yyVel;
+	public double mass;
+	public String imgFileName;
+
+	public Body(double xP, double yP, double xV, double yV, double m, String img) {
+		xxPos = xP;
+		yyPos = yP;
+		xxVel = xV;
+		yyVel = yV;
+		mass = m;
+		imgFileName = img;
+	}
+
+	public Body(Body b) {
+		xxPos = b.xxPos;
+		yyPos = b.yyPos;
+		xxVel = b.xxVel;
+		yyVel = b.yyVel;
+		mass = b.mass;
+		imgFileName = b.imgFileName;
+	}
+}
