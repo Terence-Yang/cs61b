e698d985ced17fb46b95833ea792a469b6f2b7f2
Yang Zou <yang0201@outlook.com>
2019-01-28 23:21:38 +0000

    finish the Body class

--- proj0/Body.java
+++ proj0/Body.java
@@ -6,6 +6,8 @@ public class Body {
 	public double mass;
 	public String imgFileName;
 
+	public static final double g = 6.67E-11;
+
 	public Body(double xP, double yP, double xV, double yV, double m, String img) {
 		xxPos = xP;
 		yyPos = yP;
@@ -22,5 +24,59 @@ public class Body {
 		yyVel = b.yyVel;
 		mass = b.mass;
 		imgFileName = b.imgFileName;
+	}
+
+	public double calcDistance(Body b) {
+		double dx = b.xxPos - xxPos;
+		double dy = b.yyPos - yyPos;
+		return Math.sqrt(dx * dx + dy * dy);
+	}
+
+	public double calcForceExertedBy(Body b) {
+		double r = calcDistance(b);
+		return g * mass * b.mass / (r * r);
+	}
+
+	public double calcForceExertedByX(Body b) {
+		double f = calcForceExertedBy(b);
+		double dx = b.xxPos - xxPos;
+		double r = calcDistance(b);
+		return f * dx / r;
+	}
+
+	public double calcForceExertedByY(Body b) {
+		double f = calcForceExertedBy(b);
+		double dy = b.yyPos - yyPos;
+		double r = calcDistance(b);
+		return f * dy / r;
+	}
+
+	public double calcNetForceExertedByX(Body[] bodies) {
+		double fX = 0;
+		for (Body b: bodies) {
+			if (!this.equals(b)) {
+				fX += calcForceExertedByX(b);
+			}
+		}
+		return fX;
+	}
+
+	public double calcNetForceExertedByY(Body[] bodies) {
+		double fY = 0;
+		for (Body b: bodies) {
+			if (!this.equals(b)) {
+				fY += calcForceExertedByY(b);
+			}
+		}
+		return fY;
+	}
+
+	public void update(double dt, double fX, double fY) {
+		double aX = fX / mass;
+		double aY = fY / mass;
+		xxVel += dt * aX;
+		yyVel += dt * aY;
+		xxPos += dt * xxVel;
+		yyPos += dt * yyVel; 
 	}
 }
