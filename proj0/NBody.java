d533eeebf2b3ba7c5155b2d8aa491285d85bade5
Yang Zou <yang0201@outlook.com>
2019-01-30 22:00:43 +0000

    complete proj0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -13,7 +13,7 @@ public class NBody {
 		
 		Body[] bodies = new Body[N];
 
-		for (int i=0; i < N; i++) {
+		for (int i = 0; i < N; i++) {
 			double xxPos = in.readDouble();
             double yyPos = in.readDouble();
             double xxVel = in.readDouble();
@@ -31,15 +31,49 @@ public class NBody {
 		double dt = Double.parseDouble(args[1]);
 		String filename = args[2];
 		
-		double R = readRadius(filename);
+		double radius = readRadius(filename);
 		Body[] bodies = readBodies(filename);
+		int N = bodies.length;
 
-		StdDraw.setScale(-R, R);
+		StdDraw.setScale(-radius, radius);
 		StdDraw.clear();
 		StdDraw.picture(0, 0, "images/starfield.jpg");
 
 		for (Body b: bodies) {
 			b.draw();
+		}
+
+		StdDraw.enableDoubleBuffering();
+
+		double time = 0;
+		while (time < T) {
+			double[] xForces = new double[N];
+			double[] yForces = new double[N];
+			
+			for (int i = 0; i < N; i++) {
+				xForces[i] = bodies[i].calcNetForceExertedByX(bodies);
+				yForces[i] = bodies[i].calcNetForceExertedByY(bodies);
+			}
+
+			for (int i = 0; i < N; i++) {
+				bodies[i].update(dt, xForces[i], yForces[i]);
+			}
+
+			StdDraw.picture(0, 0, "images/starfield.jpg");
+			for (Body b: bodies) {
+				b.draw();
+			}
+			StdDraw.show();
+			StdDraw.pause(10);
+			time += dt;
+		}
+
+		StdOut.printf("%d\n", bodies.length);
+		StdOut.printf("%.2e\n", radius);
+		for (int i = 0; i < bodies.length; i++) {
+		    StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+		                  bodies[i].xxPos, bodies[i].yyPos, bodies[i].xxVel,
+		                  bodies[i].yyVel, bodies[i].mass, bodies[i].imgFileName);   
 		}
 	}
 }
