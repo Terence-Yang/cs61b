2e2677e8d0c8e2aca018df3b0e3cd127b7de4552
Yang Zou <yang0201@outlook.com>
2019-02-12 23:26:12 +0000

    add wordToDeque and isPalindrome

---
+++ proj1b/Palindrome.java
@@ -0,0 +1,19 @@
+public class Palindrome {
+    public Deque<Character> wordToDeque(String word) {
+        LinkedListDeque<Character> deque = new LinkedListDeque<>();
+        for (int i = 0; i < word.length(); i++) {
+            deque.addLast(word.charAt(i));
+        }
+        return deque;
+    }
+
+    public boolean isPalindrome(String word) {
+        Deque<Character> deque = wordToDeque(word);
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -1,4 +1,4 @@
-/*import org.junit.Test;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestPalindrome {
@@ -15,4 +15,14 @@ public class TestPalindrome {
         }
         assertEquals("persiflage", actual);
     }
-}     Uncomment this class once you've created your Palindrome class. */
+
+    @Test
+    public void testIsPalindrome() {
+        assertTrue(palindrome.isPalindrome(""));
+        assertTrue(palindrome.isPalindrome("a"));
+        assertFalse(palindrome.isPalindrome("Aa"));
+        assertTrue(palindrome.isPalindrome("ABBA"));
+        assertFalse(palindrome.isPalindrome("ACDC"));
+        assertFalse(palindrome.isPalindrome("yang"));
+    }
+}
