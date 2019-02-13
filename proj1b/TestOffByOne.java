0ded7bdb7180879d1fb70e177efbdecd36079aa9
Yang Zou <yang0201@outlook.com>
2019-02-13 23:49:18 +0000

    more tests

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -8,23 +8,22 @@ public class TestOffByOne {
 
     @Test
     public void testEqualChars() {
-        assertTrue(offByOne.equalChars('x', 'y'));
-        assertTrue(offByOne.equalChars('z', 'y'));
+        assertFalse(offByOne.equalChars('a', 'a'));
         assertTrue(offByOne.equalChars('a', 'b'));
         assertTrue(offByOne.equalChars('b', 'a'));
+        assertFalse(offByOne.equalChars('a', 'c'));
+        assertFalse(offByOne.equalChars('c', 'a'));
+        assertFalse(offByOne.equalChars('A', 'A'));
         assertTrue(offByOne.equalChars('A', 'B'));
         assertTrue(offByOne.equalChars('B', 'A'));
-        assertTrue(offByOne.equalChars('1', '2'));
-        assertTrue(offByOne.equalChars('&', '%'));
-        assertTrue(offByOne.equalChars('@', 'A'));
-        assertFalse(offByOne.equalChars('a', 'a'));
-        assertFalse(offByOne.equalChars('A', 'A'));
         assertFalse(offByOne.equalChars('A', 'C'));
         assertFalse(offByOne.equalChars('C', 'A'));
-        assertFalse(offByOne.equalChars('x', 'z'));
+        assertTrue(offByOne.equalChars('%', '&'));
+        assertTrue(offByOne.equalChars('&', '%'));
+        assertFalse(offByOne.equalChars('$', '&'));
+        assertFalse(offByOne.equalChars('&', '$'));
         assertFalse(offByOne.equalChars('a', 'A'));
-        assertFalse(offByOne.equalChars('Z', 'X'));
-        assertFalse(offByOne.equalChars('1', '3'));
-        assertFalse(offByOne.equalChars('?', '!'));
+        assertFalse(offByOne.equalChars('a', '%'));
+        assertFalse(offByOne.equalChars('A', '%'));
     }
 }
