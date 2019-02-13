dd6ec4d9fb225d48ae06bed5b6baf1b10075de75
Yang Zou <yang0201@outlook.com>
2019-02-13 07:52:00 +0000

    style fix

--- proj1b/Deque.java
+++ proj1b/Deque.java
@@ -1,12 +1,12 @@
 public interface Deque<T> {
-    public void addFirst(T item);
-    public void addLast(T item);
-    public int size();
-    public void printDeque();
-    public T removeFirst();
-    public T removeLast();
-    public T get(int index);
-    default public boolean isEmpty() {
+    void addFirst(T item);
+    void addLast(T item);
+    int size();
+    void printDeque();
+    T removeFirst();
+    T removeLast();
+    T get(int index);
+    default boolean isEmpty() {
         return size() == 0;
     }
 }
