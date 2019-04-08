90292f0c2db06967d69fa45e60dd70c4540c8fe0
Yang Zou <yang0201@outlook.com>
2019-04-08 23:42:09 +0100

    fix bug and pass alternative lecture example

--- hw4/bearmaps/hw4/AStarSolver.java
+++ hw4/bearmaps/hw4/AStarSolver.java
@@ -4,36 +4,34 @@ import bearmaps.proj2ab.DoubleMapPQ;
 import bearmaps.proj2ab.ExtrinsicMinPQ;
 import edu.princeton.cs.algs4.Stopwatch;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
     private SolverOutcome outcome;
     private double solutionWeight;
     private int statesExplored;
-    private List<Vertex> solution;
+    private LinkedList<Vertex> solution;
     private double timeSpent;
 
     public AStarSolver(AStarGraph<Vertex> G, Vertex start, Vertex goal, double timeout) {
         Stopwatch sw = new Stopwatch();
-        solution = new ArrayList<>();
+        solution = new LinkedList<>();
 
-        Map<Vertex, Double> distTo= new HashMap<>();
+        Map<Vertex, Double> distTo = new HashMap<>();
+        Map<Vertex, Vertex> edgeTo = new HashMap<>();
         ExtrinsicMinPQ<Vertex> pq = new DoubleMapPQ<>();
         distTo.put(start, 0.0);
         pq.add(start, distTo.get(start) + G.estimatedDistanceToGoal(start, goal));
 
         while (pq.size() > 0 && !pq.getSmallest().equals(goal) && sw.elapsedTime() < timeout) {
             Vertex p = pq.removeSmallest();
-            solution.add(p);
             statesExplored += 1;
             for (WeightedEdge<Vertex> e : G.neighbors(p)) {
                 Vertex q = e.to();
                 double w = e.weight();
                 if (!distTo.containsKey(q) || distTo.get(p) + w < distTo.get(q)) {
                     distTo.put(q, distTo.get(p) + w);
+                    edgeTo.put(q, p);
                     if (pq.contains(q)) {
                         pq.changePriority(q, distTo.get(q) + G.estimatedDistanceToGoal(q, goal));
                     } else {
@@ -47,8 +45,12 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
             outcome = SolverOutcome.UNSOLVABLE;
         } else if (pq.getSmallest().equals(goal)) {
             outcome = SolverOutcome.SOLVED;
-            solution.add(pq.getSmallest());
-            solutionWeight = distTo.get(pq.getSmallest());
+            Vertex p = pq.getSmallest();
+            solutionWeight = distTo.get(p);
+            while (p != null) {
+                solution.addFirst(p);
+                p = edgeTo.get(p);
+            }
         } else {
             outcome = SolverOutcome.TIMEOUT;
         }
