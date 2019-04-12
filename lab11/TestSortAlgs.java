8129f1bc2107b73887fff7a1f2d22d496ad48a67
Yang Zou <yang0201@outlook.com>
2019-04-12 22:14:24 +0100

    add unit tests

--- lab11/TestSortAlgs.java
+++ lab11/TestSortAlgs.java
@@ -2,16 +2,36 @@ import edu.princeton.cs.algs4.Queue;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+
 public class TestSortAlgs {
 
     @Test
     public void testQuickSort() {
-
+        Queue<Integer> q = new Queue<>();
+        q.enqueue(2);
+        q.enqueue(3);
+        q.enqueue(0);
+        q.enqueue(8);
+        q.enqueue(5);
+        q.enqueue(0);
+        q.enqueue(1);
+        q = QuickSort.quickSort(q);
+        assertTrue(isSorted(q));
     }
 
     @Test
     public void testMergeSort() {
-
+        Queue<Integer> q = new Queue<>();
+        q.enqueue(2);
+        q.enqueue(3);
+        q.enqueue(0);
+        q.enqueue(8);
+        q.enqueue(5);
+        q.enqueue(0);
+        q.enqueue(1);
+        q = MergeSort.mergeSort(q);
+        assertTrue(isSorted(q));
     }
 
     /**
